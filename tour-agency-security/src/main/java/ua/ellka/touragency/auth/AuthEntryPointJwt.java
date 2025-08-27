01f926f6e78eaca86a0cb372a5d4ff4dbd9c1c41
eIIka <vkalkatins@gmail.com>
2025-08-27 23:02:36 +0300

    [FEATURE]: Add JWT authentication
    - Implement JWT filter, util, and entry point
    - Configure Spring Security with JWT
    - Add CustomUserDetails and service
    - Add web module

---
+++ tour-agency-security/src/main/java/ua/ellka/touragency/auth/AuthEntryPointJwt.java
@@ -0,0 +1,14 @@
+package ua.ellka.touragency.auth;
+
+import java.io.IOException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+    }
+}
